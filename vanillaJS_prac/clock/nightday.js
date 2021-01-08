07ae730b03a7d65ed29b6ad1e56550c893a96ad8
TaeHyeon Kim <kth990303@naver.com>
2021-01-08 16:18:15 +0900

    addEventListener
    1. js 파일 분리해서 야간모드 기능 추가
    2. default bgcolor: black 으로 변경

--- vanillaJS_prac/clock/nightday.js
+++ vanillaJS_prac/clock/nightday.js
@@ -1,12 +1,6 @@
-let mode=document.getElementById("bgcolor");
-mode.onclick=function(){
-    if(mode==="night"){
-        document.querySelector("body").style.backgroundColor='black';
-        document.querySelector("body").style.color='white';
-    }
-    else{
-        document.querySelector("body").style.backgroundColor='white';
-        document.querySelector("body").style.color='black';
-    }
+document.getElementById("bgblack").onclick=()=>{
+    document.querySelector("body").style.backgroundColor='black';
 }
-
+document.getElementById("bgwhite").addEventListener('click',()=>{
+    document.querySelector("body").style.backgroundColor='white';
+})
