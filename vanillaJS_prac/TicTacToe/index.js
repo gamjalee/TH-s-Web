81cc7e9d5072f5203403865a29ffbeacdac980f4
TaeHyeon Kim <kth990303@naver.com>
2021-02-06 17:40:43 +0900

    TicTacToe ver 0.6.0
    노가다로 승리인지 확인

--- vanillaJS_prac/TicTacToe/index.js
+++ vanillaJS_prac/TicTacToe/index.js
@@ -1,8 +1,8 @@
-var body=document.body;
-var table=document.createElement('table');
-var row=[];
-var col=[];
-var now='X';
+const body=document.body;
+const table=document.createElement('table');
+const row=[];
+const col=[];
+let now='X';
 // 들여쓰기가 너무 많아지므로 함수를 따로 변수로 정의
 var enter=function(event){
     // 행렬 정보 출력
@@ -16,16 +16,30 @@ var enter=function(event){
     // 칸 채워짐 여부
     if(col[rNum][cNum].textContent === ''){
         console.log('빈칸');
-        if(now==='X'){
-            // textcontent: 태그 및 상태까지 출력하고싶을 때
-            // value로 하면 출력이 되지 않고 상태만 바뀜.
-            col[rNum][cNum].textContent=now;
-            now='O';
+        // textcontent: 태그 및 상태까지 출력하고싶을 때
+        // value로 하면 출력이 되지 않고 상태만 바뀜.
+        // innerText로도 가능하긴 함.
+        // 크롬에선 거의 유사하나, 다른 브라우저에서 innerText는 공백이 생략되기도 함.
+        col[rNum][cNum].textContent=now;
+        let flag1=true, flag2=true, flag3=true, flag4=true;
+        for(let i=0;i<3;i++){
+            if(col[rNum][i].textContent!==now){
+                flag1=false;
+            }
+            if(col[i][cNum].textContent!==now){
+                flag2=false;
+            }
+            if(col[i][i].textContent!==now){
+                flag3=false;
+            }
+            if(col[i][2-i].textContent!==now){
+                flag4=false;
+            }
         }
-        else{
-            col[rNum][cNum].textContent=now;
-            now='X';
+        if(flag1 || flag2 || flag3 || flag4){
+            console.log(now, '의 승리!');
         }
+        now==='X'?now='O':now='X';
     }
     else{
         console.log('빈칸이 아님');
@@ -46,4 +60,4 @@ for(var i=0;i<3;i++){
     table.appendChild(R);
 }
 body.appendChild(table);
-console.log(line);
+console.log(col);
