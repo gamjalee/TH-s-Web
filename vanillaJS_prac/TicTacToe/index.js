de747e36768415bcd8ac09bf35adfed41eeaf402
TaeHyeon Kim <kth990303@naver.com>
2021-02-06 20:20:44 +0900

    TicTacToe ver 0.9.0

--- vanillaJS_prac/TicTacToe/index.js
+++ vanillaJS_prac/TicTacToe/index.js
@@ -2,16 +2,17 @@ const body=document.body;
 const table=document.createElement('table');
 const row=[];
 const col=[];
+let result=document.createElement('div');
 let now='X';
 // 들여쓰기가 너무 많아지므로 함수를 따로 변수로 정의
-var enter=function(event){
+let enter=function(event){
     // 행렬 정보 출력
     console.log(event.target);
     // 행 정보 출력
     console.log(event.target.parentNode);
 
-    var rNum=row.indexOf(event.target.parentNode);
-    var cNum=col[rNum].indexOf(event.target);
+    let rNum=row.indexOf(event.target.parentNode);
+    let cNum=col[rNum].indexOf(event.target);
     console.log('위치: ', rNum, cNum);
     // 칸 채워짐 여부
     if(col[rNum][cNum].textContent === ''){
@@ -37,27 +38,41 @@ var enter=function(event){
             }
         }
         if(flag1 || flag2 || flag3 || flag4){
-            console.log(now, '의 승리!');
+            result.textContent=now+'의 승리! ';
+            now='wait';
+            result.textContent+='판이 새로 시작됩니다. 기다려주십시오.'
+            setTimeout(function(){
+                now='X';
+                col.forEach(function(R){
+                    R.forEach(function(C){
+                        C.textContent='';
+                    });
+                });
+                result.textContent='';
+            }, 3000);
         }
-        now==='X'?now='O':now='X';
+        else
+            now==='X'?now='O':now='X';
     }
     else{
         console.log('빈칸이 아님');
     }
 };
 
-for(var i=0;i<3;i++){
-    var R=document.createElement('tr');
+for(let i=0;i<3;i++){
+    let R=document.createElement('tr');
     // 행 정보를 담기 위한 push
     row.push(R);
     col.push([]);
-    for(var j=0;j<3;j++){
-        var C=document.createElement('td');
-        C.addEventListener('click', enter);
+    for(let j=0;j<3;j++){
+        let C=document.createElement('td');
+        if(now!=='wait')
+            C.addEventListener('click', enter);
         R.appendChild(C);
         col[i].push(C);
     }
     table.appendChild(R);
 }
 body.appendChild(table);
+body.appendChild(result);
 console.log(col);
